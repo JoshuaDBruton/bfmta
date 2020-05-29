6b3c0aeaa31f32993ec1d6c6a4b42af643740bd1
Joshua <joshuadbruton@gmail.com>
2020-05-29 13:35:40 +0200

    Added template to explain MVC Architecture

--- bfmta/settings.py
+++ bfmta/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'bfmta.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'bfmta/templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- brighter/views.py
+++ brighter/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world.")
+    message = "Hello, world!"
+    context = {'custmessage': message}
+    return render(request, "home.html", context)
